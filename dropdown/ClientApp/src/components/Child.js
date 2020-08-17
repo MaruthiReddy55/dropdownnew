55014d03ce0cd9212abf4d52b29e1bf026dcc3db
MaruthiReddy55 <69816584+MaruthiReddy55@users.noreply.github.com>
2020-08-18 00:17:38 +0530

    Initial commit

---
+++ dropdown/ClientApp/src/components/Child.js
@@ -0,0 +1,69 @@
+﻿import React, { Component } from 'react'
+import Select from 'react-select'
+
+class Child extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showing: false,
+           
+            newArray: this.props.Countries,
+            maxNumber: this.props.maxValue
+        }
+        this.inputChange = this.inputChange.bind(this);
+    }
+
+    addCountry = () => {
+        let e = document.getElementById('searchkey').value;
+        var ss = [''];
+        var i = 0;
+        ss = this.props.Countries;
+        ss.push(e);
+        console.log(ss);
+        if (i, i < this.props.maxValue, i++) {
+
+
+        }
+        this.setState({
+            newArray: ss
+        })
+        
+    }
+    inputChange(e) {
+        let searchTerm = e.target.value;
+        this.setState({
+            newArray: this.props.Countries.filter(name => name.toLowerCase().includes(searchTerm.toLowerCase()))
+        })
+        console.log(e.target.value)
+        console.log(this.state.newArray)
+    }
+
+    viewMore=()=> {
+        var m = this.state.newArray.length;
+        this.setState({
+            maxNumber: m
+        })
+    }
+    render() {
+        const { showing } = this.state;
+        
+        return (
+            <div>
+                <button onClick={() => this.setState({ showing: !showing })}>Select a Location</button>
+                {showing?
+                    <div>
+                        <input type='text' id='searchkey' onChange={(e) => { this.inputChange(e) }} />
+                        {this.props.loggedIn ?
+                            <button onClick={this.addCountry} type="button">Add Country</button> : null}
+                        {this.state.newArray.slice(0, this.state.maxNumber).map((item) => <ul><li key={item}>{item}</li> </ul>)}
+                        <a onClick={this.viewMore}> View More</a>
+                    </div>
+                    : null
+                }
+                
+
+            </div>
+        )
+    }
+}
+export default Child
---
+++ dropdown/ClientApp/src/components/Parent.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react'
+import Child from './Child'
+
+class Parent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: false,
+            Countries: ['India', 'US', 'Singapore', 'Malaysia', 'UAE', 'France', 'Japan', 'China'],
+            maxValue: 4
+        }
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={() => this.setState({ loggedIn: true })}> Login </button><p> if you click login then you can add countries</p>
+                <Child Countries={this.state.Countries} maxValue={this.state.maxValue} loggedIn={this.state.loggedIn} />
+            </div>
+        )
+    }
+}
+export default Parent
